3b2bc18e8d544b38c4403bdb95478f43aba2e797
andiahmads <andi.fivesco@gmail.com>
2022-02-09 06:55:02 +0700

    template action

--- template_action_test.go
+++ template_action_test.go
@@ -11,6 +11,8 @@ import (
 
 /*
 golang template mendukung perintah action, seperti percabangan, perulangan dan lain-lain.
+didalam tempalate terdapat action yg bernama with yg digunakan untuk mengubah scope dot menjadi object yg kita mau.
+{{with.Value}}
 */
 
 func TemplateAction(writer http.ResponseWriter, request *http.Request) {
@@ -18,7 +20,7 @@ func TemplateAction(writer http.ResponseWriter, request *http.Request) {
 
 	t.ExecuteTemplate(writer, "if.gohtml", map[string]interface{}{
 		"Title": "template action",
-		"Name":  "",
+		"Name":  "andi",
 	})
 }
 
@@ -27,6 +29,50 @@ func TestTemplateAction(t *testing.T) {
 	recorder := httptest.NewRecorder()
 
 	TemplateAction(recorder, request)
+	response := recorder.Result()
+	body, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(body))
+}
+
+func TemplateActionRange(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/range.gohtml"))
+
+	t.ExecuteTemplate(writer, "range.gohtml", map[string]interface{}{
+		"Title": "template action Range",
+		"Hobbies": []string{
+			"Games", "kocok game", "code block",
+		},
+	})
+}
+
+func TestTemplateActionRange(t *testing.T) {
+	request := httptest.NewRequest("GET", "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateActionRange(recorder, request)
+	response := recorder.Result()
+	body, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(body))
+}
+
+func TemplateActionWith(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/with.gohtml"))
+
+	t.ExecuteTemplate(writer, "with.gohtml", map[string]interface{}{
+		"Title": "template action Range",
+		"Name":  "andi",
+		"Address": map[string]interface{}{
+			"Street": "jalan kocok",
+			"City":   "Jakarta",
+		},
+	})
+}
+
+func TestTemplateActionWith(t *testing.T) {
+	request := httptest.NewRequest("GET", "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateActionWith(recorder, request)
 	response := recorder.Result()
 	body, _ := ioutil.ReadAll(response.Body)
 	fmt.Println(string(body))
